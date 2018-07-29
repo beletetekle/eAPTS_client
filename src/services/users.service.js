d07fc96d8dfcd59e2f58b049d048c2155783d7df
Yehualashet <yehuala5355@gmail.com>
2018-07-29 17:12:44 +0300

    working on user.serivce

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,18 +1,113 @@
 import Api from './api';
 import ClientSession from './client-session.js';
 
+const pluralName = 'users';
+
 class User {
 
     static login = (email, password) => {
+        const url = pluralName + '/login?include=roles';
+        if(email && password){
+            return Api.create(url,{"email":email,"password":password,"ttl": 300000000})
+                .then((response) =>{
+                    ClientSession.storeAuth(response.data, err => {
+                        err ? console.error('cannot save session') : ''
+                    });
+                    return {
+                        success: true,
+                        message: "Logged in successfully",
+                        user: response.data
+                    }
+                })
+                .catch((error) =>{
+                    if (error.response) {
+                        if (error.response.status === 401) {
+                            return {
+                                error: true,
+                                message: "Invalid username or password"
+                            }
+                        }
+                        //todo make sure  to give correct error response rather than Oops .....
+                        return {
+                            error: true,
+                            message: "Oops error occurred please. Try Again"
+                        }
+                    }
+                    return {
+                        error: true,
+                        message: "Error: Not connected"
+                    }
 
+                });
+        }
     };
 
+    // send  email to reset password
     static sendReset = (email) => {
+        if(email){
+            return Api.create("users/reset",{"email":email})
+                .then((response) =>{
+                    return {
+                        success: true,
+                        message: "Email sent Successfully",
+                        user: response.data
+                    }
+                },
+                    error => {
+                        if (error.response) {
+                            if (error.response.status === 401) {
+                                return {
+                                    error: true,
+                                    message: "user email is not found"
+                                }
+                            }
+                            //todo make sure  to give correct error response rather than Oops .....
+                            return {
+                                error: true,
+                                message: "Oops error occurred please. Try Again"
+                            }
+                        }
+                        return {
+                            error: true,
+                            message: "Error: You are not connected!"
+                        }
 
+                    }
+
+                )
+
+        }
     };
 
     static changePassword =(password, accessToken) =>{
-        
+        // remove the existing token first  ....
+        ClientSession.removeAuth(err => {
+            err ? console.log(err) : ''
+        });
+        if(password){
+            return Api.create('users/reset-password'+accessToken,{"newPassword":password})
+                .then(
+                    response =>{
+                        return {
+                            success: true,
+                            message: "password Changed successfully",
+                            user: response.data
+                        }
+                    },
+                    //todo make sure  to give correct error response rather than Oops .....
+                    error => {
+                        return {
+                            error: true,
+                            message: "Oops error occurred please. Try Again"
+                        }
+                    }
+                ).catch(error => {
+                    return {
+                        error: true,
+                        message: "Oops error Occurred please. Try Again"
+                    }
+                });
+        }
     };
 
     static register = (values) => {
@@ -20,7 +115,17 @@ class User {
     };
 
     static logout = () => {
-       
+        ClientSession.getAccessToken(function (isLoggedIn, authData) {
+            if (isLoggedIn && authData != null) {
+                Api.create('users/logout', {})
+                    .then(response => {
+                        ClientSession.removeAuth(err => {
+                            err ? console.log(err) : ''
+                        });
+                        window.location = "#/login";
+                    });
+            }
+        });
     };
 
 }
--- src/views/user/RegisterForm.js
+++ src/views/user/RegisterForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RegisterForm = (props) => {
     return (
         <div>
-            Register
+
         </div>
     );
 };
