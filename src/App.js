e938b8ef2023c1ea84e15daa5784714bbb0302ed
Samuel Mussie <punisami@gmail.com>
2018-07-29 16:20:51 +0300

    added routes for login and register

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'simple-line-icons/css/simple-line-icons.css';
 import './scss/style.css';
 import DefaultLayout from './containers/DefaultLayout';
+import Login from './views/user/Login';
+import Register from './views/user/Register';
+import PrivateRoute from './PrivateRoute';
 
 class App extends Component {
   render() {
     return (
       <HashRouter>
         <Switch>
-          <Route path="/" name="Home" component={DefaultLayout} />
+          <Route path="/login" name="Home" component={Login} />
+          <Route path="/register" name="Home" component={Register} />
+          <PrivateRoute path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
     );
