efa21d346460ae1a999d5f773573260d563da6e1
Yehualashet <yehuala5355@gmail.com>
2018-07-30 06:53:10 +0300

    user registration but it need to be checked and validate the form using avality

--- src/views/user/RegisterForm.js
+++ src/views/user/RegisterForm.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import {Button, Card, CardHeader, CardBody, Input, Col, Container, Row, FormGroup    } from 'reactstrap';
 
 const RegisterForm = (props) => {
     return (
         <div>
-            Register
+            <Container>
+                <Card>
+                    <CardHeader> HMIS </CardHeader>
+                    <CardBody>
+                        <FormGroup>
+                            <Input type="text" onChange={props.onChange} value={props.username} name="username" placeholder="Email" required/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="email" onChange={props.onChange} value={props.email} name="email" placeholder="Email" required/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Input type="password" onChange={props.onChange} value={props.password} name="password" placeholder="password" required/>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Input type="select" name="accountTYpe" id="type" onChange={props.onChange}>
+                                <option value="0">Please select Account type</option>
+                                <option value="1">Admin </option>
+                                <option value="2">Pharmacist</option>
+                                <option value="3">Physician</option>
+                                <option value="4">patient </option>
+                            </Input>
+                        </FormGroup>
+                        <Button onClick={props.onClick}> Register </Button>
+                    </CardBody>
+                </Card>
+            </Container>
         </div>
     );
 };
